010b31db337a4c346650b9371d3f909fc2d74472
marcusotterstad <inet2@otterstad.name>
2023-04-26 17:07:01 +0200

    Added ability to import from csv

--- db.config.js
+++ db.config.js
@@ -7,82 +7,48 @@ const db = new sqlite3.Database('mydatabase.sqlite');
 db.serialize(() => {
   db.run(`
     CREATE TABLE workouts (
-      id INTEGER PRIMARY KEY,
-      date TEXT NOT NULL,
-      name TEXT NOT NULL,
-      duration INTEGER NOT NULL
-    )
-  `);
-  
-  db.run(`
-    CREATE TABLE workout_contents (
-      id INTEGER PRIMARY KEY,
-      workout_id INTEGER NOT NULL,
-      exercise_id INTEGER NOT NULL,
-      sets INTEGER NOT NULL,
-      reps INTEGER NOT NULL,
-      weight INTEGER NOT NULL,
-      FOREIGN KEY (workout_id) REFERENCES workouts(id),
-      FOREIGN KEY (exercise_id) REFERENCES exercises(id)
-    )
-  `);
+      workout_id INTEGER PRIMARY KEY AUTOINCREMENT,
+      date DATETIME,
+      workout_name VARCHAR(255),
+      set_id INTEGER REFERENCES sets(set_id),
+      duration VARCHAR(255)
+    );
+  `, function (err) {
+    if (err) {
+      console.log("Error creating workout table:", err);
+    }
+  })
 
-  db.run(`
+db.run(`
     CREATE TABLE exercises (
-      id INTEGER PRIMARY KEY,
-      name TEXT NOT NULL
-    )
-  `);
+      exercise_id INTEGER PRIMARY KEY AUTOINCREMENT,
+      exercise_name VARCHAR(255) UNIQUE
+    );
+  `, function (err) {
+  if (err) {
+    console.log("Error creating workout table:", err);
+  }
+})
 
-  db.run(`
-    CREATE TABLE daily_tasks (
-      id INTEGER PRIMARY KEY,
-      date TEXT NOT NULL,
-      habit TEXT NOT NULL,
-      completed BOOLEAN NOT NULL,
-      notes TEXT,
-      workout_id INTEGER,
-      FOREIGN KEY (workout_id) REFERENCES workouts(id)
-    )
-  `);
-  db.run(`CREATE TABLE tasks (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    description TEXT
-  );
-  ALTER TABLE daily_habits
-  ADD COLUMN task_id INTEGER REFERENCES tasks(id);
-  `)
 
+db.run(`  
+    CREATE TABLE sets (
+      set_id INTEGER PRIMARY KEY AUTOINCREMENT,
+      exercise_id INTEGER REFERENCES exercises(exercise_id),
+      set_order INTEGER,
+      weight FLOAT,
+      reps INTEGER,
+      distance FLOAT,
+      seconds INTEGER,
+      notes VARCHAR(255),
+      workout_notes VARCHAR(255),
+      rpe INTEGER
+    );
+  `, function (err) {
+  if (err) {
+    console.log("Error creating workout table:", err);
+  }
+})
 });
 
-// insert some sample data
-db.serialize(() => {
-  const workoutStmt = db.prepare(`
-    INSERT INTO workouts (date, name, duration)
-    VALUES (?, ?, ?)
-  `);
-  
-
-  const exerciseStmt = db.prepare(`
-    INSERT INTO exercises (name)
-    VALUES (?)
-  `);
-  
-  exerciseStmt.run('Bench Press');
-  exerciseStmt.run('Squats');
-  exerciseStmt.run('Deadlifts');
-
-  const habitStmt = db.prepare(`
-    INSERT INTO daily_habits (date, habit, completed, notes, workout_id)
-    VALUES (?, ?, ?, ?, ?)
-  `);
-
-  habitStmt.run('2022-01-01', 'Read for 30 minutes', true, 'Finished "The Great Gatsby"', null);
-  habitStmt.run('2022-01-02', 'Drink 8 glasses of water', false, null, null);
-  habitStmt.run('2022-01-03', 'Complete 10,000 steps', true, null, 3);
-});
-
-// close the database connection
 db.close();
-
--- workout-data-from-csv/add_workout_data.py
+++ workout-data-from-csv/add_workout_data.py
@@ -1,40 +1,46 @@
 import csv
 import sqlite3
+from datetime import datetime
 
-# connect to the database
+# Connect to the SQLite database
 conn = sqlite3.connect('../mydatabase.sqlite')
-
-# create a cursor object to execute queries
 c = conn.cursor()
 
-# open the CSV file and read its contents
-with open('strong.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
+# Open the CSV file and read the data
+with open('strong.csv') as f:
+    reader = csv.reader(f)
+    next(reader) # skip the header row
     for row in reader:
-        # insert the workout into the workouts table
-        c.execute("INSERT INTO workouts (date, name, duration) VALUES (?, ?, ?)",
-                  (row['Date'], row['Workout Name'], row['Duration']))
+        # Parse the data from the row
+        date = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S')
+        workout_name = row[1]
+        duration = row[2]
+        exercise_name = row[3]
+        set_order = int(row[4])
+        weight = float(row[5])
+        reps = int(row[6])
+        distance = float(row[7])
+        seconds = int(row[8])
+        notes = row[9]
+        workout_notes = row[10]
+        rpe = float(row[11]) if row[11] else None
+        
+        # Insert the data into the exercise table (if not already exists)
+        c.execute('INSERT OR IGNORE INTO exercises (exercise_name) VALUES (?)', (exercise_name,))
+        
+        # Get the exercise ID
+        c.execute('SELECT exercise_id FROM exercises WHERE exercise_name = ?', (exercise_name,))
+        exercise_id = c.fetchone()[0]
+        
+        # Insert the data into the set table
+        c.execute('INSERT INTO sets (exercise_id, set_order, weight, reps, distance, seconds, notes, workout_notes, rpe) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                  (exercise_id, set_order, weight, reps, distance, seconds, notes, workout_notes, rpe))
+        set_id = c.lastrowid
+        
+        # Insert the data into the workout table
+        c.execute('INSERT INTO workouts (date, workout_name, set_id, duration) VALUES (?, ?, ?, ?)',
+                  (date, workout_name, set_id, duration))
 
-        # insert the exercise into the exercises table if it doesn't already exist
-        c.execute("SELECT id FROM exercises WHERE name = ?", (row['Exercise Name'],))
-        result = c.fetchone()
-        if result is None:
-            c.execute("INSERT INTO exercises (name) VALUES (?)", (row['Exercise Name'],))
-            exercise_id = c.lastrowid
-        else:
-            exercise_id = result[0]
-
-        # insert the workout content into the workout_contents table
-        c.execute("INSERT INTO workout_contents (workout_id, exercise_id, sets, reps, weight) VALUES (?, ?, ?, ?, ?)",
-                  (c.lastrowid, exercise_id, row['Set Order'], row['Reps'], row['Weight']))
-
-        # insert the daily habit into the daily_habits table
-        c.execute("INSERT INTO daily_habits (date, habit, completed, notes, workout_id) VALUES (?, ?, ?, ?, ?)",
-                  (row['Date'], 'Exercise', True, row['Notes'], c.lastrowid))
-
-# commit the changes to the database
+# Commit the changes and close the database connection
 conn.commit()
-
-# close the cursor and the connection
-c.close()
 conn.close()
